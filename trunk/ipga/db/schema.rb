a12c3093ef8d63c4d8cf5e8d5c029e29af3ecde5
spritesun <spritesun@01969a64-5e2b-11de-8dc1-e798e446b60a>
2009-06-21 10:40:39 +0000

    spritesun & add basic login function

--- trunk/ipga/db/schema.rb
+++ trunk/ipga/db/schema.rb
@@ -9,13 +9,24 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090621033534) do
+ActiveRecord::Schema.define(:version => 20090621083614) do
 
   create_table "resources", :force => true do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "crypted_password",          :limit => 40
+    t.string   "salt",                      :limit => 40
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "remember_token"
+    t.datetime "remember_token_expires_at"
   end
 
 end
