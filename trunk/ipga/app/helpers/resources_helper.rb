7a1d5074220bcab96b321a018fde6ecb395bc7a0
spritesun <spritesun@01969a64-5e2b-11de-8dc1-e798e446b60a>
2009-06-27 04:03:01 +0000

    spritesun & add more fields for resources

--- trunk/ipga/app/helpers/resources_helper.rb
+++ trunk/ipga/app/helpers/resources_helper.rb
@@ -1,2 +1,8 @@
 module ResourcesHelper
+  ActionView::Helpers::FormBuilder.class_eval do
+    def reference_data_collection_select object_name, options = {}
+      text_method = options[:text_method] || :name
+      collection_select "#{object_name}_id", object_name.to_s.camelize.constantize.find(:all), :id, text_method, :prompt => :true
+    end
+  end
 end
--- trunk/ipga/app/models/resource.rb
+++ trunk/ipga/app/models/resource.rb
@@ -1,3 +1,8 @@
 class Resource < ActiveRecord::Base
   belongs_to :location
+  belongs_to :strength
+  belongs_to :department
+  belongs_to :level
+  belongs_to :official_grade
+  belongs_to :industry
 end
