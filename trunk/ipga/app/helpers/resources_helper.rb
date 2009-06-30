b5174eb032f48631c2d87007041271df6bc315bd
spritesun <spritesun@01969a64-5e2b-11de-8dc1-e798e446b60a>
2009-06-30 00:49:15 +0000

    spritesun - add basic search function according to location

--- trunk/ipga/app/helpers/resources_helper.rb
+++ trunk/ipga/app/helpers/resources_helper.rb
@@ -2,7 +2,20 @@ module ResourcesHelper
   ActionView::Helpers::FormBuilder.class_eval do
     def reference_data_collection_select object_name, options = {}
       text_method = options[:text_method] || :name
-      collection_select "#{object_name}_id", object_name.to_s.camelize.constantize.find(:all), :id, text_method, :prompt => :true
+      collection_select "#{object_name}_id", object_name.to_s.camelize.constantize.find(:all), :id, text_method, :prompt => true
     end
   end
+
+  def collection_select_tag method, collection, value_method, text_method, options = {}, html_options = {}
+    option_tags = options[:prompt] ? content_tag(:option, options[:prompt].kind_of?(String) ? options[:prompt] : 'Please select', :value => '') : ''
+    option_tags << collection.collect{ |item| content_tag :option, item.send(text_method), :value => item.send(value_method) }.join('')
+    select_tag method, option_tags, html_options
+  end
+
+  # TODO: sunlong - i don't think this method is neccessary, but still finding a way to fix it out.
+  def reference_data_collection_select_tag object_name, options = {}
+    text_method = options[:text_method] || :name
+    collection_select_tag "#{object_name}_id", object_name.to_s.camelize.constantize.find(:all), :id, text_method, :prompt => true
+  end
+
 end
--- trunk/ipga/spec/controllers/resources_controller_spec.rb
+++ trunk/ipga/spec/controllers/resources_controller_spec.rb
@@ -1,10 +1,15 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe ResourcesController do
-  fixtures :locations, :strengths, :departments, :levels, :industries, :official_grades
+
+  fixtures :locations, :strengths, :departments, :levels, :industries, :official_grades, :resources
+
+  before :each do
+    login_as User.first
+  end
+
   describe 'POST create' do
     it 'creates resource' do
-      login_as User.first
       lambda do
         post :create, :resource => {:description => 'some thing', :location_id => Location.first.id, :strength_id => Strength.first.id,
                                     :industry_id => Industry.first.id, :department_id => Department.first.id, :level_id => Level.first.id,
@@ -12,6 +17,13 @@ describe ResourcesController do
       end.should change(Resource, :count).by(1)
 
       response.should be_redirect
+    end
+  end
+
+  describe 'GET search' do
+    it 'search keywords by partially match of location name' do
+      get :search, :keyword => 'uha'
+      assigns[:results].should == [Resource.first]
     end
   end
 end
