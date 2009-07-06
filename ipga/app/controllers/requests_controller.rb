ea886ac88659da9f04802188207cecc34e6c6d48
spritesun <spritesun@01969a64-5e2b-11de-8dc1-e798e446b60a>
2009-07-06 12:55:34 +0000

    spritesun - do not list my resource in search results

--- ipga/app/controllers/requests_controller.rb
+++ ipga/app/controllers/requests_controller.rb
@@ -6,15 +6,6 @@ class RequestsController < ApplicationController
 
   # GET /requests/create
   def create
-    #@request = Request.new(params[:request])
-    #
-    #if @request.save
-    #  flash[:notice] = 'Request was successfully created.'
-    #  render :text => 'Request was successfully created.'
-    #else
-    #  render :text => 'Request was not successfully created.'
-    #end
-
     @request = Request.new
     @request.sender_id= self.current_user.id
     @request.resource_id= params[:resource_id]
@@ -22,8 +13,8 @@ class RequestsController < ApplicationController
     if @request.save
       render :text => 'Request was successfully created.'
     else
+      p @request.errors
       render :text => 'Request was not successfully created.'
     end
-
   end
 end
