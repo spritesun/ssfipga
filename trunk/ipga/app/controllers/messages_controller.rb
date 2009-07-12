382fe9c33de76e03245fac393d1f86a4a0af8b10
spritesun <spritesun@01969a64-5e2b-11de-8dc1-e798e446b60a>
2009-07-12 10:55:08 +0000

    flanker & spritesun - send request message

--- trunk/ipga/app/controllers/messages_controller.rb
+++ trunk/ipga/app/controllers/messages_controller.rb
@@ -1,6 +1,17 @@
 class MessagesController < ApplicationController
   def new
-    @message = Message.new(:request => Request.new(:resource => Resource.find(params[:resource_id])))
+    @message = Message.new
+    @resource_id = params[:resource_id]
     render_greybox
+  end
+
+  def create
+    @message = Message.new(:request => Request.new(:resource => Resource.find(params[:resource_id]), :sender => current_user))
+    @message.attributes = params[:message]
+    if @message.save
+      render :text => '发送成功'
+    else
+      render_greybox :action => :new
+    end
   end
 end
