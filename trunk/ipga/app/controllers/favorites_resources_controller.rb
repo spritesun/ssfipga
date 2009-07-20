7d564c67ffaf17c64d24bdf10a3260b2e5cc78c0
flankerfc <flankerfc@01969a64-5e2b-11de-8dc1-e798e446b60a>
2009-07-20 15:55:14 +0000

    flanker: remove favorite function

--- trunk/ipga/app/controllers/favorites_resources_controller.rb
+++ trunk/ipga/app/controllers/favorites_resources_controller.rb
@@ -5,4 +5,11 @@ class FavoritesResourcesController < ApplicationController
     @favorites_resource = FavoritesResource.create(:resource => Resource.find(params[:resource_id]), :favorite => current_user.favorite)
     render :partial => 'shared/favorite', :locals => { :favorite => current_user.favorite }
   end
+
+  # DELETE /resources/1
+  def destroy
+    @favorites_resource = FavoritesResource.find(params[:id])
+    @favorites_resource.destroy
+    render :partial => 'shared/favorite', :locals => { :favorite => current_user.favorite }
+  end
 end
